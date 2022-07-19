f1b65f069b2d3bc3a2d54515cf161fe5d0deef43
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-07-19 16:46:57 +0200

    Update index.html and add some js  in index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const myFunction = () => {
-    let x = document.getElementById("myLinks");
-    if (x.style.display === 'block') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}
+const element = document.getElementById("nav-button");
+element.addEventListener("click", () => {
+  let x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    element.innerHTML = '<I class="fa fa-bars"></I>';
+    element.style.color = "white";
+    x.style.display = "none";
+  } else {
+    element.innerHTML = '<I class="fa fa-times-circle"></I>';
+    element.style.color = "red";
+    x.style.display = "block";
+  }
+});
