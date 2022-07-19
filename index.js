f79732313731e76c24eeea8649259b244bde4daf
AduwuJoseph <aduwujoseph4real@gmail.com>
2022-07-19 16:08:02 +0100

    modify mobile menu css to align with design

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const element = document.getElementById("nav-button");
-element.addEventListener("click", () => {
+const element = document.getElementById('nav-button');
+element.addEventListener('click', () => {
   let x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
+  if (x.style.display === 'block') {
     element.innerHTML = '<I class="fa fa-bars"></I>';
-    element.style.color = "white";
-    x.style.display = "none";
-  } else {
+    element.classList.remove('text-danger');
+    x.style.display = 'none';
+  } 
+  else {
     element.innerHTML = '<I class="fa fa-times-circle"></I>';
-    element.style.color = "red";
-    x.style.display = "block";
+    element.classList.add('text-danger');
+    x.style.display = 'block';
   }
 });
