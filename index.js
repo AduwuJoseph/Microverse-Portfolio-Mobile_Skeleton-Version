10d59d6aa906b9d8b76a5ec514fb5c15a5af8395
AduwuJoseph <aduwujoseph4real@gmail.com>
2022-07-19 16:19:03 +0100

    fix linters errors

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const element = document.getElementById('nav-button');
 element.addEventListener('click', () => {
-  let x = document.getElementById("myLinks");
+  const x = document.getElementById('myLinks');
   if (x.style.display === 'block') {
     element.innerHTML = '<I class="fa fa-bars"></I>';
     element.classList.remove('text-danger');
     x.style.display = 'none';
-  } 
-  else {
+  } else {
     element.innerHTML = '<I class="fa fa-times-circle"></I>';
     element.classList.add('text-danger');
     x.style.display = 'block';
