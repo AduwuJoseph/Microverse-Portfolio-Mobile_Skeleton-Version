cd9ca750674e0977533d8c9799d55f9a0656c315
AduwuJoseph <aduwujoseph4real@gmail.com>
2022-07-21 00:29:13 +0100

    Required changes fixed

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const element = document.getElementById('nav-button');
+const contact = document.getElementsByClassName('desktop-only');
 element.addEventListener('click', () => {
   const x = document.getElementById('myLinks');
   if (x.style.display === 'block') {
     element.innerHTML = '<I class="fa fa-bars"></I>';
     element.classList.remove('text-danger');
     x.style.display = 'none';
+    contact.style.display = 'none';
   } else {
     element.innerHTML = '<I class="fa fa-times-circle"></I>';
     element.classList.add('text-danger');
     x.style.display = 'block';
+    contact.style.display = 'block';
   }
 });
