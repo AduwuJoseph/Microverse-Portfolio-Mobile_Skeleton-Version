d2a4af48d61de9457f4a42f9164116b0a4e7fae7
AduwuJoseph <aduwujoseph4real@gmail.com>
2022-07-21 00:32:18 +0100

    Required changes fixed for menu icon

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ element.addEventListener('click', () => {
   const x = document.getElementById('myLinks');
   if (x.style.display === 'block') {
     element.innerHTML = '<I class="fa fa-bars"></I>';
-    element.classList.remove('text-danger');
     x.style.display = 'none';
     contact.style.display = 'none';
   } else {
-    element.innerHTML = '<I class="fa fa-times-circle"></I>';
-    element.classList.add('text-danger');
+    element.innerHTML = '<I class="fa fa-times"></I>';
     x.style.display = 'block';
     contact.style.display = 'block';
   }
