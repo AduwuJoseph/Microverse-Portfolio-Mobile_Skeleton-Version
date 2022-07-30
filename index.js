2027e9bd6ba9d12198aedfcd868729f3116f7167
AduwuJoseph <aduwujoseph4real@gmail.com>
2022-07-30 15:04:50 +0100

    Format JS file

--- index.js
+++ index.js
@@ -126,9 +126,9 @@ const emailCheckLowerCase = (emailtxt) => {
   }
   return result;
 };
+
 submitbtn.onclick = () => {
   if (emailCheckLowerCase(email.value) === true) {
     myform.submit();
   }
 };
-
